14aed2e50f31f7cc08697fce80c8334146ee8b7f
seungchanKKK <eccsck@gmail.com>
2023-01-01 23:59:47 +0900

    알고리즘 실행 환경변경

--- src/main/java/com/example/algo/beakjoon/b1.java
+++ src/main/java/com/example/algo/beakjoon/b1.java
@@ -2,7 +2,7 @@ package com.example.algo.beakjoon;
 
 import java.util.Scanner;
 
-public class Main
+public class b1
 {
     public static void main(String[] args)
     {Scanner sc = new Scanner(System.in);
