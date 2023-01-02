824037b3591b841fce918308448265c0f29d1d4c
SeungchanKKK <eccsck@gmail.com>
2023-01-02 23:52:05 +0900

    [백준] 배열 최소,최대 풀이 오답

---
+++ src/main/java/com/example/algo/beakjoon/b3.java
@@ -0,0 +1,19 @@
+package com.example.algo.beakjoon;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class b3 {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int[] array = new int[A];
+
+        for(int i=0; i<A;i++){
+            array[i]= sc.nextInt();
+        }
+        
+        System.out.println(Arrays.stream(array).min()+" "+Arrays.stream(array).max());
+    }
+}
