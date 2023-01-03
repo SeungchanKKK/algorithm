13a486dd98a78b72b2c5eaa4d8bb3c112f5bde63
DESKTOP-8FOR6JP\User <eccsck@gmail.com>
2023-01-03 11:48:04 +0900

    [백준] 배열 최대값 정답제출

--- src/main/java/com/example/algo/beakjoon/b4.java
+++ src/main/java/com/example/algo/beakjoon/b4.java
@@ -2,6 +2,7 @@ package com.example.algo.beakjoon;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class b4 {
@@ -14,9 +15,10 @@ public class b4 {
             array.add(sc.nextInt());
         }
 
-        int mm = array.stream().max().get();
+        int mm = Collections.max(array);
         int idx = array.indexOf(mm);
 
-        System.out.println(Arrays.stream(array).max().getAsInt());
+        System.out.println(mm);
+        System.out.println(idx);
     }
 }
