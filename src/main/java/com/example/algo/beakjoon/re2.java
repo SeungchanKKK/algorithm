a1ee9cb430339088dded4352ed537d76d761051f
DESKTOP-8FOR6JP\User <eccsck@gmail.com>
2023-01-04 11:06:11 +0900

    [백준] 반복문 빠른 A+B 정답제출

--- src/main/java/com/example/algo/beakjoon/re2.java
+++ src/main/java/com/example/algo/beakjoon/re2.java
@@ -1,6 +1,7 @@
 package com.example.algo.beakjoon;
 
 import java.io.*;
+import java.util.StringTokenizer;
 
 // 빠른 A+B
 // https://m.blog.naver.com/ka28/221850826909 참조링크
@@ -10,11 +11,17 @@ public class re2 {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(bf.readLine());
-        for (int i =0; i<N; i++){
-            int A = Integer.parseInt(bf.readLine());
-            int B = Integer.parseInt(bf.readLine());
-            bw.write(A+B);
-        }
 
+        StringTokenizer st;
+
+        for (int i =0; i<N; i++){
+            st = new StringTokenizer(bf.readLine()," ");
+            int A = Integer.parseInt(st.nextToken());
+            int B =Integer.parseInt(st.nextToken());
+            bw.write(A+B+"\n");
+        }
+        bf.close();
+        bw.flush();
+        bw.close();
     }
 }
