3e8e140ca75b7e5c619ef3b1adad902245d17f57
DESKTOP-8FOR6JP\User <eccsck@gmail.com>
2023-01-05 12:01:49 +0900

    [백준] 반복문 별찍기2 정답제출

--- src/main/java/com/example/algo/beakjoon/re5.java
+++ src/main/java/com/example/algo/beakjoon/re5.java
@@ -3,13 +3,16 @@ package com.example.algo.beakjoon;
 import java.io.IOException;
 import java.util.Scanner;
 
-//별찍기1
-public class re4 {
+//별찍기2
+public class re5 {
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
 
         for (int i=1; i<=N;i++){
+            for (int k =0; k<N-i; k++){
+                System.out.print(" ");
+            }
             for (int j=0; j<i;j++){
                 System.out.print("*");
             }
