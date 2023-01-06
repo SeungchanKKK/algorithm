2ca7a373d466f05457fc05218d397d4998fa5d6d
DESKTOP-8FOR6JP\User <eccsck@gmail.com>
2023-01-06 12:06:12 +0900

    [백준] 함수 한수 재귀용법수정 정답제출

--- src/main/java/com/example/algo/beakjoon/class3.java
+++ src/main/java/com/example/algo/beakjoon/class3.java
@@ -1,11 +1,12 @@
 package com.example.algo.beakjoon;
 
 import java.util.Scanner;
-
+//한수 재귀용법 적용
 public class class3 {
     public static void main(String[] args){
         class3 class3 = new class3();
         class3.out();
+        //System.out.println(class3.hanCheck(210,11));
     }
 
     public void out(){
@@ -13,25 +14,28 @@ public class class3 {
         int N = sc.nextInt();
         int answer=0;
         for (int i=1; i<=N; i++){
-            if(hanCheck(i)){
+            if(hanCheck(i,11)){
                 answer++;
             }
         }
         System.out.println(answer);
     }
 
-    public boolean hanCheck(int a){
-        if(a<100){
+    public boolean hanCheck(int a,int gap){
+        if(a<100&&gap==11){
             return true;
-        } else if (a==1000) {
-            return false;
         } else {
-            int gap= (a/10)%10-a%10;
-            int gap2 =(a/10)/10%10-(a/10)%10;
-            if(gap2==gap){
+            int gapTemp= (a/10)%10-a%10;
+            if(gap!=11){
+                if(gapTemp!=gap){
+                    return false;
+                }
+            }
+            int num= a/10;
+            if(num/10==0){
                 return true;
             }
-            return false;
+            return hanCheck(num,gapTemp);
         }
     }
 }
