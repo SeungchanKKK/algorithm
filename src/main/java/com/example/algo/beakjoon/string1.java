72ab64ebf2d336b28c63790e16ff0743a2d5eb40
DESKTOP-8FOR6JP\User <eccsck@gmail.com>
2023-01-06 14:45:08 +0900

    [백준]문자열 아스키코드 정답제출

---
+++ src/main/java/com/example/algo/beakjoon/string1.java
@@ -0,0 +1,13 @@
+package com.example.algo.beakjoon;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+//아스키코드
+public class string1 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String letter = sc.nextLine();
+        System.out.println(Arrays.toString(letter.getBytes()).replace("]","").replace("[",""));
+    }
+}
