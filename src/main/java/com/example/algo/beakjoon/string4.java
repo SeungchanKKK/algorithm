6590f11fcc4a8787e09e139c4f174350f46d2f52
seungchanKKK <eccsck@gmail.com>
2023-01-07 23:58:05 +0900

    [백준] 문자열 문자열반복 정답제출

---
+++ src/main/java/com/example/algo/beakjoon/string4.java
@@ -0,0 +1,20 @@
+package com.example.algo.beakjoon;
+
+import java.util.Scanner;
+
+public class string4 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for (int i =0; i<N;i++){
+            int len = sc.nextInt();
+            String word = sc.next();
+            for (int j =0; j<word.length(); j++){
+                for (int k=0; k<len;k++){
+                    System.out.print(word.charAt(j));
+                }
+            }
+            System.out.println();
+        }
+    }
+}
