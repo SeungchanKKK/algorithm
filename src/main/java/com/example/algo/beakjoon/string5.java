2da55ca16224795362e5d974edfda5b6193df7d2
seungchanKKK <eccsck@gmail.com>
2023-01-08 00:55:53 +0900

    [백준] 문자열 단어공부 정답제출

---
+++ src/main/java/com/example/algo/beakjoon/string5.java
@@ -0,0 +1,45 @@
+package com.example.algo.beakjoon;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class string5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String word = sc.next();
+        String arrayed = word.toLowerCase();
+        String[] alpha = arrayed.split("");
+        Arrays.sort(alpha);
+        ArrayList<String> alphabet = new ArrayList<>();
+        for (int i = 97; i < 123; i++) {
+            alphabet.add(String.valueOf((char) i));
+        }
+        ArrayList<Integer> count = new ArrayList<>();
+        for(int i =0; i<alphabet.size(); i++){
+            count.add(0);
+        }
+        for (int i = 0; i < alpha.length; i++) {
+            int idx = alphabet.indexOf(alpha[i]);
+            int nVal= count.get(idx)+1;
+            count.set(idx,nVal );
+        }
+        int large= 0;
+        boolean check = false;
+        for (int i=0; i< count.size(); i++){
+            if(count.get(i)>large){
+                large= count.get(i);
+                check = true;
+            } else if (count.get(i)==large) {
+                check = false;
+            }
+        }
+        if(check){
+            System.out.println(alphabet.get(count.indexOf(large)).toUpperCase());
+        }else {
+            System.out.println("?");
+        }
+
+    }
+}
+
