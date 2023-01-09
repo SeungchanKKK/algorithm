ce3abbcec227c1589d4e1d721e997520d804a4db
DESKTOP-8FOR6JP\User <eccsck@gmail.com>
2023-01-09 09:27:26 +0900

    [백준] 문제 이름 주석달기

--- src/main/java/com/example/algo/beakjoon/string4.java
+++ src/main/java/com/example/algo/beakjoon/string4.java
@@ -1,7 +1,7 @@
 package com.example.algo.beakjoon;
 
 import java.util.Scanner;
-
+//문자열 반복
 public class string4 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
--- src/main/java/com/example/algo/beakjoon/string5.java
+++ src/main/java/com/example/algo/beakjoon/string5.java
@@ -3,7 +3,7 @@ package com.example.algo.beakjoon;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
-
+//단어공부
 public class string5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
