8fc2fb4def4b65ff2a6b15c8200112aa9ac5ee4c
DESKTOP-8FOR6JP\User <eccsck@gmail.com>
2023-01-10 11:31:41 +0900

    [백준] 2775 부녀회장이 될테야 정답제출

---
+++ src/main/java/com/example/algo/beakjoon/math4.java
@@ -0,0 +1,37 @@
+package com.example.algo.beakjoon;
+
+import java.util.Scanner;
+//부녀회장이 될테야
+public class math4 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for (int i=0; i<N; i++){
+            int floor = sc.nextInt();
+            int room = sc.nextInt();
+        int people = roomNum(floor,room);
+            System.out.println(people);
+        }
+    }
+
+    public static int roomNum(int floor,int room){
+        int[][] cache = new int[floor+1][room];
+        cache[0][0]=1;
+        if(room>1){
+            cache[0][1]=2;
+        }
+        cache[1][0]=1;
+        for(int i=0; i<floor+1;i++){
+            for (int j=0; j<room;j++){
+                cache[i][0]=1;
+                cache[0][j]=j+1;
+            }
+        }
+        for (int i=1; i<floor+1;i++){
+            for(int j=1; j<room;j++){
+                cache[i][j]=cache[i][j-1]+cache[i-1][j];
+            }
+        }
+        return cache[floor][room-1];
+    }
+}
