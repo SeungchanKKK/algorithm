2f75dc92bc2cd7f10eff327180fa7e0e5d375422
DESKTOP-8FOR6JP\User <eccsck@gmail.com>
2023-01-11 16:44:07 +0900

    [백준] 1929 소수구하기 주석추가

--- src/main/java/com/example/algo/beakjoon/n1929.java
+++ src/main/java/com/example/algo/beakjoon/n1929.java
@@ -1,7 +1,7 @@
 package com.example.algo.beakjoon;
 
 import java.util.Scanner;
-
+//소수구하기
 public class n1929 {
     public static void main(String[] args){
         Scanner sc= new Scanner(System.in);
