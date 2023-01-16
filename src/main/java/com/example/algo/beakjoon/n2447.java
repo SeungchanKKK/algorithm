3d81d4eeb74e512e76af981db8a94020e33bd222
DESKTOP-8FOR6JP\User <eccsck@gmail.com>
2023-01-16 09:25:58 +0900

    [백준] 2447 별찍기-10 문제풀이중

---
+++ src/main/java/com/example/algo/beakjoon/n2447.java
@@ -0,0 +1,19 @@
+package com.example.algo.beakjoon;
+
+import java.util.Scanner;
+//별찍기 -10
+public class n2447 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        System.out.println(Star("*",N));
+    }
+    public static String Star(String s,int N){
+        if(N==1){
+            return s;
+        }else {
+
+            return Star(star,N/3);
+        }
+    }
+}
