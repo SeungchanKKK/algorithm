c34c3c53395543d689dcfde90ca26d96f6d942f8
DESKTOP-8FOR6JP\User <eccsck@gmail.com>
2023-02-03 17:53:54 +0900

    [프로그래머스] lv2 예상대진표 정답제출

---
+++ src/main/java/com/example/algo/programmers/MatchList.java
@@ -0,0 +1,17 @@
+package com.example.algo.programmers;
+
+public class MatchList {
+    public int solution(int n, int a, int b)
+    {
+        int answer = 0;
+        while (true){
+            a=a/2+a%2;
+            b=b/2+b%2;
+            answer++;
+            if(a==b){
+                break;
+            }
+        }
+        return answer;
+    }
+}
