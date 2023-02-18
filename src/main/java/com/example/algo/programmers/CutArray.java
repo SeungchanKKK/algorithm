392ee51e3e5dd4424ae5f5771f6523ac65900b16
seungchanKKK <eccsck@gmail.com>
2023-02-18 21:38:41 +0900

    [프로그래머스] lv2 n^2배열 자르기 풀이중

---
+++ src/main/java/com/example/algo/programmers/CutArray.java
@@ -0,0 +1,11 @@
+package com.example.algo.programmers;
+
+public class CutArray {
+    public int[] solution(int n, long left, long right) {
+        int[] answer = {};
+        long step = left/n+1;
+        long arr = left%n+1;
+
+        return answer;
+    }
+}
