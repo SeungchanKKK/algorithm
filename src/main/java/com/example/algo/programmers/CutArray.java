03c83a7b5ec8dc23cb78051a6580a2c014786fff
DESKTOP-8FOR6JP\User <eccsck@gmail.com>
2023-02-21 11:49:29 +0900

    [프로그래머스] lv2 n^2배열 자르기 정답제출

--- src/main/java/com/example/algo/programmers/CutArray.java
+++ src/main/java/com/example/algo/programmers/CutArray.java
@@ -1,12 +1,15 @@
 package com.example.algo.programmers;
 
+import java.util.ArrayList;
+
 public class CutArray {
     public int[] solution(int n, long left, long right) {
-        int[] answer = {};
-        long step = left/n+1;
-        long arr = left%n+1;
-
-        
+        int[] answer = new int[(int) (right-left)];
+        int idx=0;
+        for(long i=left; i<=right; i++){
+            answer[idx]= (int) Math.max(i/n+1,i%n+1);
+            idx++;
+        }
         return answer;
     }
 }
