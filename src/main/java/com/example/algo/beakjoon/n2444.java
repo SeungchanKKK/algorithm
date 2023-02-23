574917824732924ab472b8fe15bdded09e0c8d41
DESKTOP-8FOR6JP\User <eccsck@gmail.com>
2023-02-23 18:06:56 +0900

    [백준] 2444 별찍기7 풀이중

--- src/main/java/com/example/algo/beakjoon/n2444.java
+++ src/main/java/com/example/algo/beakjoon/n2444.java
@@ -6,6 +6,11 @@ public class n2444 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-
+        int repeat = 2*N-1;
+        for(int i=1; i<=repeat;i++){
+            for (int j=1; j<=repeat;j++){
+                if()
+            }
+        }
     }
 }
