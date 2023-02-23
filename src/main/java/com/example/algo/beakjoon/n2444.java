abe53a8ce7ae6c77dc357ee14879e2d5d26b9d19
DESKTOP-8FOR6JP\User <eccsck@gmail.com>
2023-02-23 17:51:51 +0900

    [백준] 2444 별찍기7 풀이중

---
+++ src/main/java/com/example/algo/beakjoon/n2444.java
@@ -0,0 +1,11 @@
+package com.example.algo.beakjoon;
+
+import java.util.Scanner;
+
+public class n2444 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+    }
+}
