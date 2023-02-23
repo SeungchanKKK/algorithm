99acf4c2ffc9086de51eb1c2c85ef21ecff55a3b
DESKTOP-8FOR6JP\User <eccsck@gmail.com>
2023-02-23 17:37:42 +0900

    [백준] 2743 단어길이재기 정답제출

---
+++ src/main/java/com/example/algo/beakjoon/n2743.java
@@ -0,0 +1,10 @@
+package com.example.algo.beakjoon;
+
+import java.util.Scanner;
+
+public class n2743 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextLine().length());
+    }
+}
