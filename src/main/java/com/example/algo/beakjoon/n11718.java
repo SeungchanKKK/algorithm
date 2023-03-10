f811d35efecd6724f54359664634ef44b6c32acc
DESKTOP-8FOR6JP\User <eccsck@gmail.com>
2023-03-10 11:30:15 +0900

    [백준]  11718 그대로 출력하기 정답제출

---
+++ src/main/java/com/example/algo/beakjoon/n11718.java
@@ -0,0 +1,12 @@
+package com.example.algo.beakjoon;
+
+import java.util.Scanner;
+//그대로 출력하기
+public class n11718 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            System.out.println(sc.nextLine());
+        }
+    }
+}
