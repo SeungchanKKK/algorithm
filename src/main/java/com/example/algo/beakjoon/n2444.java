a4c5dbf9531b23192b85056b1cf3289fd2973538
DESKTOP-8FOR6JP\User <eccsck@gmail.com>
2023-03-10 14:50:40 +0900

    [백준]  2444 별찍기-7 정답제출

--- src/main/java/com/example/algo/beakjoon/n2444.java
+++ src/main/java/com/example/algo/beakjoon/n2444.java
@@ -1,15 +1,27 @@
 package com.example.algo.beakjoon;
 
 import java.util.Scanner;
-
+//별찍기-7
 public class n2444 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        int repeat = 2*N-1;
-        for(int i=1; i<=repeat;i++){
-            for (int j=1; j<=repeat;j++){
+        int repeat = 2 * N - 1;
+        for (int i = 1; i <= repeat; i++) {
+            for (int j = 1; j <= repeat; j++) {
+                int a=0;
+                if(i<=N){
+                    a=i;
+                }else {
+                    a=2*N-i;
+                }
+                if (Math.abs(N - j) < a) {
+                    System.out.print("*");
+                } else if(N-j>0) {
+                    System.out.print(" ");
+                }
             }
+            System.out.println();
         }
     }
 }
