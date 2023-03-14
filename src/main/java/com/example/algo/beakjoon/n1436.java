7f2acb7ad23e998ed53131bee2efb007df498d7b
DESKTOP-8FOR6JP\User <eccsck@gmail.com>
2023-03-14 16:56:53 +0900

    [백준]  1434 영화감독 숌 풀이중

---
+++ src/main/java/com/example/algo/beakjoon/n1436.java
@@ -0,0 +1,12 @@
+package com.example.algo.beakjoon;
+
+import java.util.Scanner;
+//영화 감독 숌
+public class n1436 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int answer=666;
+        while ()
+    }
+}
