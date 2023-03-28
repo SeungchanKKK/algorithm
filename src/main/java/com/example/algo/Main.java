959c63ba6ba6f4d115b7e94d7269cae3d13e35c2
DESKTOP-8FOR6JP\User <eccsck@gmail.com>
2023-03-28 17:54:59 +0900

    [프로그래머스] lv1 덧칠하기 정답제출

--- src/main/java/com/example/algo/Main.java
+++ src/main/java/com/example/algo/Main.java
@@ -1,18 +1,14 @@
 package com.example.algo;
 
-import com.example.algo.programmers.MatrixMultiplication;
-import com.example.algo.programmers.RotatingParentheses;
-import com.example.algo.programmers.RoughKeyboard;
-
-import java.util.Arrays;
+import com.example.algo.programmers.OverPainting;
 
 public class Main {
     public static void main(String[] ars) {
 
-        RoughKeyboard roughKeyboard = new RoughKeyboard();
-        String[] keymap ={"AGZ", "BSSS"};
-        String[] targets={"ASA","BGZ"};
-        System.out.println(Arrays.toString(roughKeyboard.solution(keymap, targets)));
+        OverPainting overPainting = new OverPainting();
+        int[] sec ={2, 3, 6};
+        int a= overPainting.solution(8,4,sec);
+        System.out.println(a);
 
     }
 }
---
+++ src/main/java/com/example/algo/programmers/OverPainting.java
@@ -0,0 +1,28 @@
+package com.example.algo.programmers;
+
+import java.util.ArrayList;
+
+public class OverPainting {
+        public int solution(int n, int m, int[] section) {
+            int answer = 0;
+            ArrayList<Integer>wall = new ArrayList<>();
+            for (int i=0; i<n;i++){
+                wall.add(1);
+            }
+            for (int problem:section){
+                wall.set(problem-1,0);
+            }
+            while (wall.contains(0)){
+                answer++;
+                int startPoint = wall.indexOf(0);
+                for (int i=0; i<m;i++){
+                    if(startPoint>=wall.size()){
+                        break;
+                    }
+                    wall.set(startPoint,1);
+                    startPoint++;
+                }
+            }
+            return answer;
+        }
+}
