657cf2b0d7ced2a11c9c3a359ffb7e2ace663ff6
DESKTOP-8FOR6JP\User <eccsck@gmail.com>
2023-04-04 15:34:24 +0900

    [프로그래머스] lv2 연속 부분 수열 합의 개수 풀이중

---
+++ src/main/java/com/example/algo/programmers/ConsecutiveSequenceSums.java
@@ -0,0 +1,25 @@
+package com.example.algo.programmers;
+
+import java.util.ArrayList;
+//연속 부분 수열 합의 개수
+public class ConsecutiveSequenceSums {
+    public int solution(int[] elements) {
+        int answer = 0;
+        ArrayList<Integer>sums = new ArrayList<>();
+        for (int i=1; i<=elements.length; i++){
+
+        }
+        return answer;
+    }
+
+    public int sums(int reps, int[] arr){
+        int rep=0;
+        for (int i=rep; i<arr.length;i++){
+            int sum=0;
+            if (rep==reps){
+                return sum;
+            }
+        }
+        return 0;
+    }
+}
