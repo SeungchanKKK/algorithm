c967739b2494e29138ab885894612e1ab593340e
DESKTOP-8FOR6JP\User <eccsck@gmail.com>
2023-04-11 17:55:39 +0900

    [프로그래머스] lv1 달리기시합 풀이중

---
+++ src/main/java/com/example/algo/programmers/RunningRace.java
@@ -0,0 +1,24 @@
+package com.example.algo.programmers;
+
+import java.util.HashMap;
+
+public class RunningRace {
+    public String[] solution(String[] players, String[] callings) {
+        HashMap<String,Integer>ranking = new HashMap<>();
+        for (int i =0; i<players.length;i++){
+            ranking.put(players[i],i);
+        }
+        for (String name:callings){
+            overPass(ranking,players,name);
+        }
+        return players;
+    }
+    public void overPass(HashMap<String,Integer>ranking,String[] players, String player){
+        int position = ranking.get(player);
+        String passed = players[position-1];
+        players[position-1] = player;
+        players[position] = passed;
+        ranking.put(player,position-1);
+        ranking.put(passed,position+1);
+    }
+}
