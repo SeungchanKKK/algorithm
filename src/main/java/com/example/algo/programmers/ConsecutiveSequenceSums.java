e4f35639292ab86ac3f663166f861e64110fcf80
DESKTOP-8FOR6JP\User <eccsck@gmail.com>
2023-04-13 17:10:52 +0900

    [프로그래머스] lv2 연속 부분 수열 합의 개수 정답제출

--- src/main/java/com/example/algo/programmers/ConsecutiveSequenceSums.java
+++ src/main/java/com/example/algo/programmers/ConsecutiveSequenceSums.java
@@ -1,25 +1,31 @@
 package com.example.algo.programmers;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+
 //연속 부분 수열 합의 개수
 public class ConsecutiveSequenceSums {
     public int solution(int[] elements) {
-        ArrayList<Integer>sums = new ArrayList<>();
+        int answer=0;
+        HashMap<Integer,Boolean> sums = new HashMap<>();
         for (int i=0; i<elements.length; i++){
-            getSum(i,elements,sums);
+            answer+=getSum(i,elements,sums);
         }
-        return sums.size();
+        return answer;
     }
 
-    public void getSum(int reps, int[] arr, ArrayList<Integer>sums) {
+    public int getSum(int reps, int[] arr, HashMap<Integer,Boolean>sums) {
+        int cnt =0;
         for (int i = 0; i <= arr.length; i++) {
             int sum =0;
             for (int j = 0; j <= reps; j++) {
             sum+= arr[(i+j)%arr.length];
             }
-            if (!sums.contains(sum)){
-                sums.add(sum);
+            if (!sums.containsKey(sum)){
+                sums.put(sum,true);
+                cnt++;
             }
         }
+        return cnt;
     }
 }
