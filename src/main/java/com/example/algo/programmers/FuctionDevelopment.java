1c12ccd6583e03722f008e41a1913a43bccc5476
DESKTOP-8FOR6JP\User <eccsck@gmail.com>
2023-04-17 17:51:00 +0900

    [프로그래머스] lv2 기능개발 풀이중

--- src/main/java/com/example/algo/programmers/FuctionDevelopment.java
+++ src/main/java/com/example/algo/programmers/FuctionDevelopment.java
@@ -1,8 +1,19 @@
 package com.example.algo.programmers;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class FuctionDevelopment {
     public int[] solution(int[] progresses, int[] speeds) {
         int[] answer = {};
+        Queue<Integer> system = new LinkedList<>();
+        for (int progress: progresses){
+            system.add(progress);
+        }
+        while (!system.isEmpty()){
+            int i =0;
+            
+        }
         return answer;
     }
 }
