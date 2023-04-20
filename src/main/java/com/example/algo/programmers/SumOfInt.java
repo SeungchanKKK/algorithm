dfdb714a3737696fd8de4871a55165ed6e3e5006
DESKTOP-8FOR6JP\User <eccsck@gmail.com>
2023-04-20 16:39:01 +0900

    [프로그래머스] lv0 소문자로바꾸기 정답제출

--- src/main/java/com/example/algo/programmers/SumOfInt.java
+++ src/main/java/com/example/algo/programmers/SumOfInt.java
@@ -1,14 +1,7 @@
 package com.example.algo.programmers;
 
 public class SumOfInt {
-    public int solution(int a, int b) {
-        int answer = 0;
-        int sum =Integer.parseInt(String.valueOf(a)+String.valueOf(b));
-        int multiply = a*b*2;
-        if (sum>=multiply){
-            return sum;
-        }else {
-            return multiply;
-        }
+    public String solution(String myString) {
+        return myString.toLowerCase();
     }
 }
