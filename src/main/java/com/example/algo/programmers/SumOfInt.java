71e89ced3a0b130510dba1b056de2124de8b6066
DESKTOP-8FOR6JP\User <eccsck@gmail.com>
2023-04-24 15:19:37 +0900

    [프로그래머스] lv0 a와 b 출력하기 정답제출

--- src/main/java/com/example/algo/programmers/SumOfInt.java
+++ src/main/java/com/example/algo/programmers/SumOfInt.java
@@ -1,13 +1,18 @@
 package com.example.algo.programmers;
 
+import java.util.Arrays;
+
 public class SumOfInt {
-    public int[] solution(int n, int k) {
-        int[] answer = new int[n/k];
-        int factor = k;
-        for (int i=0;i<n/k;i++){
-            answer[i]=k;
-            k+=k;
+    public int[] solution(int[] arr) {
+        for (int i=0;i<arr.length;i++){
+            int num = arr[i];
+            if(num>=50&&num%2==0){
+                num/=2;
+            } else if (num<50&&num%2==1) {
+                num*=2;
+            }
+            arr[i]=num;
         }
-        return answer;
+        return arr;
     }
 }
