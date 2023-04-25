831e016bd1269d48309ba6de4a6dc3121576b4bd
DESKTOP-8FOR6JP\User <eccsck@gmail.com>
2023-04-25 14:31:15 +0900

    [프로그래머스] lv0 문자열 겹쳐쓰기 정답제출

--- src/main/java/com/example/algo/programmers/SumOfInt.java
+++ src/main/java/com/example/algo/programmers/SumOfInt.java
@@ -3,13 +3,15 @@ package com.example.algo.programmers;
 import java.util.Scanner;
 
 public class SumOfInt {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        if(n%2==0){
-            System.out.println(n+" is even");
-        }else {
-            System.out.println(n+" is odd");
+    public String solution(String my_string, String overwrite_string, int s) {
+        String answer = "";
+        for (int i=0; i<my_string.length();i++){
+            if(i>=s&&i<overwrite_string.length()+s-1){
+                answer+=overwrite_string.charAt(i-s);
+            }else {
+                answer+=my_string.charAt(i);
+            }
         }
+        return answer;
     }
 }
