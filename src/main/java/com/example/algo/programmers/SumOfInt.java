f94da4ca9f6035a938dddc9f548993cfbec95eed
DESKTOP-8FOR6JP\User <eccsck@gmail.com>
2023-04-27 11:58:56 +0900

    [프로그래머스] lv0 등차수열의 특정한 항만 더하기 정답제출

--- src/main/java/com/example/algo/programmers/SumOfInt.java
+++ src/main/java/com/example/algo/programmers/SumOfInt.java
@@ -1,29 +1,14 @@
 package com.example.algo.programmers;
 
 public class SumOfInt {
-    public String solution(String code) {
-        StringBuilder answer = new StringBuilder();
-        int mode =0;
-        for (int i=0; i<code.length(); i++){
-            if(mode==0){
-                if(code.charAt(i)=='1'){
-                    mode=1;
-                }
-                if(i%2==0&&code.charAt(i)!='1'){
-                    answer.append(code.charAt(i));
-                }
-            }else {
-                if(code.charAt(i)=='1'){
-                    mode=0;
-                }
-                if(i%2==1&&code.charAt(i)!='1'){
-                    answer.append(code.charAt(i));
-                }
+    public int solution(int a, int d, boolean[] included) {
+        int answer = 0;
+        for (int i=0; i<included.length;i++){
+            if (included[i]){
+                answer+=a;
             }
+            a+=d;
         }
-        if (answer.length()==0){
-            return "EMPTY";
-        }
-        return answer.toString();
+        return answer;
     }
 }
