527075d91df73d01ae2afa9dd4969aa55917fc90
DESKTOP-8FOR6JP\User <eccsck@gmail.com>
2023-04-27 18:36:35 +0900

    [프로그래머스] lv0 수열과 구간 쿼리 2 정답제출

--- src/main/java/com/example/algo/programmers/SumOfInt.java
+++ src/main/java/com/example/algo/programmers/SumOfInt.java
@@ -1,20 +1,21 @@
 package com.example.algo.programmers;
 
 public class SumOfInt {
-    public int solution(int a, int b, int c) {
-        int answer = 0;
-        if(a==b){
-            if(a==c){
-                return (a+b+c)*(int)(Math.pow(a,2)+Math.pow(b,2)+Math.pow(c,2))*(int)(Math.pow(a,3)+Math.pow(b,3)+Math.pow(c,3));
-            }else {
-                return (a+b+c)*(int)(Math.pow(a,2)+Math.pow(b,2)+Math.pow(c,2));
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = new int[queries.length];
+        for (int i =0; i< queries.length;i++){
+            int min = 1000001;
+            for (int j=queries[i][0];j<=queries[i][1];j++){
+                if(arr[j]>queries[i][2]&&arr[j]<min){
+                    min=arr[j];
+                }
             }
-        }else {
-            if(b==c||a==c){
-                return (a+b+c)*(int)(Math.pow(a,2)+Math.pow(b,2)+Math.pow(c,2));
+            if(min==1000001){
+                answer[i]=-1;
             }else {
-                return a+b+c;
+                answer[i]=min;
             }
         }
+        return answer;
     }
 }
