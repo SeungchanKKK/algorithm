f58ba092d291619455716561d3105d3f3bf37aa1
DESKTOP-8FOR6JP\User <eccsck@gmail.com>
2023-04-27 12:40:48 +0900

    [프로그래머스] lv0 주사위 게임 2 정답제출

--- src/main/java/com/example/algo/programmers/SumOfInt.java
+++ src/main/java/com/example/algo/programmers/SumOfInt.java
@@ -1,14 +1,20 @@
 package com.example.algo.programmers;
 
 public class SumOfInt {
-    public int solution(int a, int d, boolean[] included) {
+    public int solution(int a, int b, int c) {
         int answer = 0;
-        for (int i=0; i<included.length;i++){
-            if (included[i]){
-                answer+=a;
+        if(a==b){
+            if(a==c){
+                return (a+b+c)*(int)(Math.pow(a,2)+Math.pow(b,2)+Math.pow(c,2))*(int)(Math.pow(a,3)+Math.pow(b,3)+Math.pow(c,3));
+            }else {
+                return (a+b+c)*(int)(Math.pow(a,2)+Math.pow(b,2)+Math.pow(c,2));
             }
-            a+=d;
+        }else {
+            if(b==c||a==c){
+                return (a+b+c)*(int)(Math.pow(a,2)+Math.pow(b,2)+Math.pow(c,2));
+            }else {
+                return a+b+c;
+            }
         }
-        return answer;
     }
 }
