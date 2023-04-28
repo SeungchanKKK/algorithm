bdf959df0a6fa5f54e0666e0a315dd5e3f556cd9
DESKTOP-8FOR6JP\User <eccsck@gmail.com>
2023-04-28 11:14:16 +0900

    [프로그래머스] lv0 A 강조하기 정답제출

--- src/main/java/com/example/algo/programmers/SumOfInt.java
+++ src/main/java/com/example/algo/programmers/SumOfInt.java
@@ -1,21 +1,11 @@
 package com.example.algo.programmers;
 
+import java.util.ArrayList;
+
 public class SumOfInt {
-    public int[] solution(int[] arr, int[][] queries) {
-        int[] answer = new int[queries.length];
-        for (int i =0; i< queries.length;i++){
-            int min = 1000001;
-            for (int j=queries[i][0];j<=queries[i][1];j++){
-                if(arr[j]>queries[i][2]&&arr[j]<min){
-                    min=arr[j];
-                }
-            }
-            if(min==1000001){
-                answer[i]=-1;
-            }else {
-                answer[i]=min;
-            }
-        }
-        return answer;
+    public String solution(String myString) {
+        myString=myString.toLowerCase();
+        myString=myString.replace("a","A");
+        return myString;
     }
 }
