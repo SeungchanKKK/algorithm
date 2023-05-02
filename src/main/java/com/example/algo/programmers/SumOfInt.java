908b406f74429cafddc557f6987fc0bbff5fcdfb
DESKTOP-8FOR6JP\User <eccsck@gmail.com>
2023-05-02 11:59:57 +0900

    [프로그래머스] lv0 콜라츠 수열 만들기 정답제출

--- src/main/java/com/example/algo/programmers/SumOfInt.java
+++ src/main/java/com/example/algo/programmers/SumOfInt.java
@@ -1,16 +1,22 @@
 package com.example.algo.programmers;
 
+import java.util.ArrayList;
+
 public class SumOfInt {
-    public int[][] solution(int n) {
-        int[][] answer = new int[n][n];
-        for (int i=0; i<n;i++){
-            for (int j=0; j<n; j++){
-                if(i==j){
-                    answer[i][j]=1;
-                }else {
-                    answer[i][j]=0;
-                }
+    public int[] solution(int n) {
+        ArrayList<Integer>array = new ArrayList<>();
+        array.add(n);
+        while (n!=1){
+            if(n%2==0){
+                n/=2;
+            }else {
+                n=n*3+1;
             }
+            array.add(n);
+        }
+        int[] answer = new int[array.size()];
+        for (int i=0; i<answer.length; i++){
+            answer[i]=array.get(i);
         }
         return answer;
     }
