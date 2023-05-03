8d00900d0603a2adfecd2f67ba51badd0a79fc17
DESKTOP-8FOR6JP\User <eccsck@gmail.com>
2023-05-03 17:36:20 +0900

    [프로그래머스] lv0 9로 나눈 나머지 정답제출

--- src/main/java/com/example/algo/programmers/SumOfInt.java
+++ src/main/java/com/example/algo/programmers/SumOfInt.java
@@ -1,31 +1,11 @@
 package com.example.algo.programmers;
 
-import java.util.ArrayList;
-
 public class SumOfInt {
-    public int[] solution(int[] arr) {
-        ArrayList<Integer>array = new ArrayList<>();
-        int i=0;
-        while (true){
-            if (array.isEmpty()){
-                array.add(arr[i]);
-                i++;
-            }else {
-                if(array.get(array.size()-1)<arr[i]){
-                    array.add(arr[i]);
-                    i++;
-                    if(arr.length==i){
-                        break;
-                    }
-                }else {
-                    array.remove(array.size()-1);
-                }
-            }
+    public int solution(String number) {
+        int answer = 0;
+        for (int i=0; i<number.length();i++){
+            answer+=Integer.parseInt(String.valueOf(number.charAt(i)));
         }
-        int[] stk = new int[array.size()];
-        for (int j=0; j<stk.length; j++){
-            stk[j]=array.get(j);
-        }
-        return stk;
+        return answer%9;
     }
 }
