b9e4ce942100ac5dbccb47b639a815b6ff8969b4
DESKTOP-8FOR6JP\User <eccsck@gmail.com>
2023-05-04 14:29:11 +0900

    [프로그래머스] lv0 문자열 여러 번 뒤집기 정답제출

--- src/main/java/com/example/algo/programmers/SumOfInt.java
+++ src/main/java/com/example/algo/programmers/SumOfInt.java
@@ -1,11 +1,22 @@
 package com.example.algo.programmers;
 
 public class SumOfInt {
-    public int solution(String number) {
-        int answer = 0;
-        for (int i=0; i<number.length();i++){
-            answer+=Integer.parseInt(String.valueOf(number.charAt(i)));
+    public String solution(String my_string, int[][] queries) {
+        String answer = my_string;
+        for (int[] querie:queries){
+            answer= switching(answer,querie[0],querie[1]);
         }
-        return answer%9;
+        return answer;
+    }
+
+    public String switching(String word,int a,int b){
+        String part = word.substring(a,b+1);
+        StringBuilder front = new StringBuilder(word.substring(0, a));
+        String behind = word.substring(b+1);
+        for (int i=part.length()-1;i>=0;i--){
+            front.append(part.charAt(i));
+        }
+        front.append(behind);
+        return String.valueOf(front);
     }
 }
