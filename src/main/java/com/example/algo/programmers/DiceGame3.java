d0c14d3be37e7c6299fb1aefac1ae60ae6eb567d
DESKTOP-8FOR6JP\User <eccsck@gmail.com>
2023-05-08 15:48:14 +0900

    [프로그래머스] lv0 뒤에서 5등까지 정답제출

--- src/main/java/com/example/algo/programmers/DiceGame3.java
+++ src/main/java/com/example/algo/programmers/DiceGame3.java
@@ -3,14 +3,11 @@ package com.example.algo.programmers;
 import java.util.Arrays;
 
 public class DiceGame3 {
-    public int[] solution(int[] arr) {
-        int[] answer = new int[Arrays.stream(arr).sum()];
-        int idx=0;
-        for (int num:arr){
-            for (int i=0; i<num;i++){
-                answer[idx]=num;
-                idx++;
-            }
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[5];
+        Arrays.sort(num_list);
+        for (int i=0; i<5; i++){
+            answer[i]=num_list[i];
         }
         return answer;
     }
