ca97558f9eff34e0cadf955c4c72aabc7745f9c3
DESKTOP-8FOR6JP\User <eccsck@gmail.com>
2023-05-08 17:08:25 +0900

    [프로그래머스] lv0 배열의 원소 삭제하기 정답제출

--- src/main/java/com/example/algo/programmers/DiceGame3.java
+++ src/main/java/com/example/algo/programmers/DiceGame3.java
@@ -1,12 +1,24 @@
 package com.example.algo.programmers;
 
+import java.util.ArrayList;
+
 public class DiceGame3 {
-    public int[] solution(int[] num_list, int n) {
-        int[] answer = new int[num_list.length];
-        int idx =n-1;
-        for (int i=0; i<num_list.length;i++){
-            answer[i]=num_list[idx% num_list.length];
-            
+    public int[] solution(int[] arr, int[] delete_list) {
+        ArrayList<Integer>numList = new ArrayList<>();
+        for (int num:arr){
+            boolean check= true;
+            for (int delete:delete_list){
+                if(num==delete){
+                    check=false;
+                }
+            }
+            if(check){
+                numList.add(num);
+            }
+        }
+        int[] answer = new int[numList.size()];
+        for (int i=0; i< answer.length; i++){
+            answer[i]= numList.get(i);
         }
         return answer;
     }
