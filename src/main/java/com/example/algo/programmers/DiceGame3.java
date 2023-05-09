3b74e1bd37610b95a70608f38cb096ab903abbe9
DESKTOP-8FOR6JP\User <eccsck@gmail.com>
2023-05-09 15:24:54 +0900

    [프로그래머스] lv0 세로 읽기 정답제출

--- src/main/java/com/example/algo/programmers/DiceGame3.java
+++ src/main/java/com/example/algo/programmers/DiceGame3.java
@@ -1,15 +1,11 @@
 package com.example.algo.programmers;
 
 public class DiceGame3 {
-    public String solution(String my_string, int s, int e) {
-        StringBuilder start = new StringBuilder(my_string.substring(0, s - 1));
-        String middle = my_string.substring(s,e);
-        String end = my_string.substring(e);
-        System.out.println(middle);
-        for (int i=middle.length()-1;i>=0;i++){
-            start.append(middle.charAt(i));
+    public String solution(String my_string, int m, int c) {
+        String answer = "";
+        for (int i=c-1; i<my_string.length()-1;i+=m){
+            answer+=my_string.charAt(i);
         }
-        start.append(end);
-        return String.valueOf(start);
+        return answer;
     }
 }
