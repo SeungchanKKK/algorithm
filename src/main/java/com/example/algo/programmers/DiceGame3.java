5b9667f16d9ac8a42922bd701831deadd21b0071
DESKTOP-8FOR6JP\User <eccsck@gmail.com>
2023-05-09 14:54:09 +0900

    [프로그래머스] lv0 배열 만들기 5 정답제출

--- src/main/java/com/example/algo/programmers/DiceGame3.java
+++ src/main/java/com/example/algo/programmers/DiceGame3.java
@@ -3,22 +3,18 @@ package com.example.algo.programmers;
 import java.util.ArrayList;
 
 public class DiceGame3 {
-    public int[] solution(int[] arr, int[] delete_list) {
+    public int[] solution(String[] intStrs, int k, int s, int l) {
         ArrayList<Integer>numList = new ArrayList<>();
-        for (int num:arr){
-            boolean check= true;
-            for (int delete:delete_list){
-                if(num==delete){
-                    check=false;
-                }
-            }
-            if(check){
-                numList.add(num);
+        for (String str:intStrs){
+            String amount=str.substring(s,l);
+            System.out.println(amount);
+            if (Integer.parseInt(amount)>k){
+                numList.add(Integer.parseInt(amount));
             }
         }
         int[] answer = new int[numList.size()];
-        for (int i=0; i< answer.length; i++){
-            answer[i]= numList.get(i);
+        for (int i=0; i<answer.length; i++){
+            answer[i]=numList.get(i);
         }
         return answer;
     }
