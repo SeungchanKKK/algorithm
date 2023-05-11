a78b49c31b6f5fc62b5e36dd688d422b10187b56
DESKTOP-8FOR6JP\User <eccsck@gmail.com>
2023-05-11 16:44:51 +0900

    [프로그래머스] lv0 문자 개수 세기 정답제출

--- src/main/java/com/example/algo/programmers/DiceGame3.java
+++ src/main/java/com/example/algo/programmers/DiceGame3.java
@@ -1,18 +1,16 @@
 package com.example.algo.programmers;
 
 public class DiceGame3 {
-    public int solution(int[] num_list) {
-        int answer = 0;
-        for (int num: num_list){
-            while (num!=1){
-                if(num%2==0){
-                    num/=2;
-                    answer++;
-                }else {
-                    num=(num-1)/2;
-                    answer++;
-                }
+    public int[] solution(String my_string) {
+        int[] answer = new int[52];
+        for (int i=0; i<my_string.length(); i++){
+            int idx =0;
+            if(my_string.charAt(i)<91&&64<my_string.charAt(i)){
+                idx =my_string.charAt(i)-65;
+            }else {
+                idx = my_string.charAt(i)-97+26;
             }
+            answer[idx]++;
         }
         return answer;
     }
