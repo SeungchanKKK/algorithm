e71fb9c3501a83f0725454497e7d008b46628d07
DESKTOP-8FOR6JP\User <eccsck@gmail.com>
2023-05-11 18:17:49 +0900

    [프로그래머스] lv0 리스트 자르기 풀이중

--- src/main/java/com/example/algo/programmers/DiceGame3.java
+++ src/main/java/com/example/algo/programmers/DiceGame3.java
@@ -1,19 +1,40 @@
 package com.example.algo.programmers;
 
 public class DiceGame3 {
-    public String solution(String my_string, int[] indices) {
-        StringBuilder answer = new StringBuilder();
-        for (int i=0; i<my_string.length();i++){
-            boolean check = true;
-            for (int indice:indices){
-                if (i==indice){
-                    check= false;
-                }
-            }
-            if (check){
-                answer.append(my_string.charAt(i));
-            }
+    public int[] solution(int n, int[] slicer, int[] num_list) {
+        int[] answer = {};
+        int start = 0;
+        int end = 0;
+        int gap = 1;
+        switch (n) {
+            case 1:
+                start = 0;
+                end = slicer[1];
+                break;
+            case 2:
+                start = slicer[0];
+                end = num_list.length-1;
+                break;
+            case 3:
+                start = slicer[0];
+                end = slicer[1];
+                break;
+            case 4:
+                start = slicer[0];
+                end = slicer[1];
+                gap = slicer[2];
+                break;
         }
-        return answer.toString();
+        return arrayset(start,end,gap,num_list);
+    }
+
+    public int[] arrayset(int start,int end, int gap,int[]num_list){
+        int[]result = new int[(end-start)/gap+1];
+        int idx=0;
+        for (int i=0; i<num_list.length; i+=gap){
+            result[idx]=num_list[i];
+            idx++;
+        }
+        return result;
     }
 }
