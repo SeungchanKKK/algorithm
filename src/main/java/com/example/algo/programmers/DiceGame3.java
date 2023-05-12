9d67dbb4dffc6f9d8585f90349b8ae23135eeb0c
DESKTOP-8FOR6JP\User <eccsck@gmail.com>
2023-05-12 16:12:41 +0900

    [프로그래머스] lv0 2의 영역 정답제출

--- src/main/java/com/example/algo/programmers/DiceGame3.java
+++ src/main/java/com/example/algo/programmers/DiceGame3.java
@@ -1,41 +1,33 @@
 package com.example.algo.programmers;
 
 public class DiceGame3 {
-    public int[] solution(int n, int[] slicer, int[] num_list) {
+    public int[] solution(int[] arr) {
+        int startIndex = -1;
+        int endIndex = -1;
+        for (int i=0; i<arr.length;i++){
+            if(arr[i]==2){
+                startIndex=i;
+                break;
+            }
+        }
+        for (int i=arr.length-1;i>=0;i--){
+            if(arr[i]==2){
+                endIndex=i;
+                break;
+            }
+        }
         int[] answer = {};
-        int start = 0;
-        int end = 0;
-        int gap = 1;
-        switch (n) {
-            case 1:
-                start = 0;
-                end = slicer[1];
-                break;
-            case 2:
-                start = slicer[0];
-                end = num_list.length-1;
-                break;
-            case 3:
-                start = slicer[0];
-                end = slicer[1];
-                break;
-            case 4:
-                start = slicer[0];
-                end = slicer[1];
-                gap = slicer[2];
-                break;
+        if (startIndex==-1&&endIndex==-1){
+            answer=new int[1];
+            answer[0]=-1;
+            return answer;
+        }else {
+            answer=new int[endIndex-startIndex+1];
+            int idx=0;
+            for (int i = startIndex; i<=endIndex;i++){
+                answer[idx]= arr[i];
+            }
         }
-        return arrayset(start,end,gap,num_list);
-    }
-
-    public int[] arrayset(int start,int end, int gap,int[]num_list){
-        int[]result = new int[(end-start)/gap+1];
-        System.out.println(result.length);
-        int idx=0;
-        for (int i=0; i<num_list.length; i+=gap){
-            result[idx]=num_list[i];
-            idx++;
-        }
-        return result;
+        return answer;
     }
 }
