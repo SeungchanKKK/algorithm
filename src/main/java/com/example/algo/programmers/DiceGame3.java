66f45129513915706d3c269da21ac9db69e953f0
DESKTOP-8FOR6JP\User <eccsck@gmail.com>
2023-05-16 10:05:38 +0900

    [프로그래머스] lv0 수열과 구간 쿼리 1 정답제출

--- src/main/java/com/example/algo/programmers/DiceGame3.java
+++ src/main/java/com/example/algo/programmers/DiceGame3.java
@@ -1,26 +1,12 @@
 package com.example.algo.programmers;
 
 public class DiceGame3 {
-    public String[] solution(String[] str_list) {
-        String[] answer = {};
-        for (int i=0; i<str_list.length; i++){
-            if (str_list[i].equals("l")){
-                answer=new String[i];
-                for (int j=0; j<answer.length; j++){
-                    answer[j]= str_list[j];
-                }
-                break;
-            }
-            if (str_list[i].equals("r")){
-                answer=new String[str_list.length-i-1];
-                int idx=0;
-                for (int j=i+1; j<=answer.length; j++){
-                    answer[idx]= str_list[j];
-                    idx++;
-                }
-                break;
+    public int[] solution(int[] arr, int[][] queries) {
+        for (int[] query:queries){
+            for (int i=query[0];i<query[1];i++){
+                arr[i]++;
             }
         }
-        return answer;
+        return arr;
     }
 }
