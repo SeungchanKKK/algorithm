1d6b563585322693cec53b091f813703b016053a
DESKTOP-8FOR6JP\User <eccsck@gmail.com>
2023-05-16 10:45:12 +0900

    [프로그래머스] lv0 조건에 맞게 수열 변환하기 2 정답제출

--- src/main/java/com/example/algo/programmers/DiceGame3.java
+++ src/main/java/com/example/algo/programmers/DiceGame3.java
@@ -1,12 +1,27 @@
 package com.example.algo.programmers;
 
 public class DiceGame3 {
-    public int[] solution(int[] arr, int[][] queries) {
-        for (int[] query:queries){
-            for (int i=query[0];i<query[1];i++){
-                arr[i]++;
+    public int solution(int[] arr) {
+        int answer = 0;
+        while (true){
+            boolean check = true;
+            for (int i=0; i<arr.length;i++){
+                int num = arr[i];
+                if(num>=50&&num%2==0){
+                    arr[i]=num/=2;
+                    check= false;
+                }
+                if (num<50&&num%2==1){
+                    arr[i]=num*2+1;
+                    check= false;
+                }
+            }
+            if (!check){
+                answer++;
+            }else {
+                break;
             }
         }
-        return arr;
+        return answer;
     }
 }
