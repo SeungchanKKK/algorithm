dc717d9f7a6d6686372ea6b70f7b78dc78118682
DESKTOP-8FOR6JP\User <eccsck@gmail.com>
2023-05-16 15:42:58 +0900

    [프로그래머스] lv0 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기 정답제출

--- src/main/java/com/example/algo/programmers/DiceGame3.java
+++ src/main/java/com/example/algo/programmers/DiceGame3.java
@@ -1,25 +1,12 @@
 package com.example.algo.programmers;
 
 public class DiceGame3 {
-    public int solution(int[] arr) {
-        int answer = 0;
-        while (true){
-            boolean check = true;
-            for (int i=0; i<arr.length;i++){
-                int num = arr[i];
-                if(num>=50&&num%2==0){
-                    arr[i]=num/=2;
-                    check= false;
-                }
-                if (num<50&&num%2==1){
-                    arr[i]=num*2+1;
-                    check= false;
-                }
-            }
-            if (!check){
-                answer++;
-            }else {
-                break;
+    public String solution(String myString, String pat) {
+        String answer = "";
+        for (int i=myString.length()-1; i-pat.length()>=0; i--){
+            String part = myString.substring(i-pat.length(),i);
+            if(part.equals(pat)){
+                return myString.substring(0,i);
             }
         }
         return answer;
