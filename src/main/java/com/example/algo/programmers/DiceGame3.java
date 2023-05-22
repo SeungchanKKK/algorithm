cd21194736e3d45a66c09f2aa8a0c3caec4c2663
DESKTOP-8FOR6JP\User <eccsck@gmail.com>
2023-05-22 15:21:17 +0900

    [프로그래머스] lv0 무작위로 K개의 수 뽑기 정답제출

--- src/main/java/com/example/algo/programmers/DiceGame3.java
+++ src/main/java/com/example/algo/programmers/DiceGame3.java
@@ -1,30 +1,26 @@
 package com.example.algo.programmers;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class DiceGame3 {
-    public int[] solution(int[] arr) {
-        ArrayList<Integer> stk = new ArrayList<>();
-        for (int i = 0; i < arr.length; i++) {
-            if (stk.size() == 0) {
-                stk.add(arr[i]);
-            } else {
-                if (stk.get(stk.size() - 1) == arr[i]) {
-                    stk.remove(stk.size() - 1);
-                } else {
-                    stk.add(arr[i]);
+    public int[] solution(int[] arr, int k) {
+        int[] answer = {};
+        ArrayList<Integer>intList = new ArrayList<>();
+        for (int i=0; i<arr.length; i++){
+            if(intList.size()>=k){
+                break;
+            }else {
+                if (!intList.contains(arr[i])){
+                    intList.add(arr[i]);
                 }
             }
         }
-        int[]answer = {};
-        if (stk.size()==0){
-            answer=new int[1];
-            answer[0]=1;
-        }else {
-            answer = new int[stk.size()];
-            for (int i=0; i<answer.length;i++){
-                answer[i]=stk.get(i);
+        answer= new int[k];
+        for (int i=0; i<k; i++){
+            if(i+1>intList.size()){
+                answer[i]=-1;
+            }else {
+                answer[i]=intList.get(i);
             }
         }
         return answer;
