5db0ea3c01e020e15624ee60f804e5d425e14369
DESKTOP-8FOR6JP\User <eccsck@gmail.com>
2023-05-22 15:02:23 +0900

    [프로그래머스] lv0 배열 만들기 6 정답제출

--- src/main/java/com/example/algo/Main.java
+++ src/main/java/com/example/algo/Main.java
@@ -2,11 +2,14 @@ package com.example.algo;
 
 import com.example.algo.programmers.DiceGame3;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] ars) {
 
         DiceGame3 roughKeyboard = new DiceGame3();
-        System.out.println(roughKeyboard.solution(1,1,1,4));
+        int[] arr = {0, 1, 1, 1, 0};
+        System.out.println(Arrays.toString(roughKeyboard.solution(arr)));
 
     }
 }
--- src/main/java/com/example/algo/programmers/DiceGame3.java
+++ src/main/java/com/example/algo/programmers/DiceGame3.java
@@ -1,25 +1,31 @@
 package com.example.algo.programmers;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class DiceGame3 {
-    public int[] solution(int[] arr, boolean[] flag) {
-        Deque<Integer> numStack= new ArrayDeque<>();
-        for (int i=0; i<arr.length; i++){
-            if (flag[i]){
-                for (int j=0; j<arr[i]*2;j++){
-                    numStack.push(arr[i]);
-                }
-            }else {
-                for (int j=0; j<arr[i];j++){
-                    numStack.pop();
+    public int[] solution(int[] arr) {
+        ArrayList<Integer> stk = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (stk.size() == 0) {
+                stk.add(arr[i]);
+            } else {
+                if (stk.get(stk.size() - 1) == arr[i]) {
+                    stk.remove(stk.size() - 1);
+                } else {
+                    stk.add(arr[i]);
                 }
             }
         }
-        int[] answer = new int[numStack.size()];
-        for (int i=0; i<answer.length; i++){
-            answer[i]=numStack.pollFirst();
+        int[]answer = {};
+        if (stk.size()==0){
+            answer=new int[1];
+            answer[0]=1;
+        }else {
+            answer = new int[stk.size()];
+            for (int i=0; i<answer.length;i++){
+                answer[i]=stk.get(i);
+            }
         }
         return answer;
     }
