e71c890b349887b8f60919d360d5899bcd9800f5
seungchanKKK <eccsck@gmail.com>
2023-08-31 22:09:07 +0900

    [백준]  11650 좌표 정렬하기 List->배열수정후 정답제출

--- src/main/java/com/example/algo/beakjoon/N11650.java
+++ src/main/java/com/example/algo/beakjoon/N11650.java
@@ -1,32 +1,31 @@
 package com.example.algo.beakjoon;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Scanner;
+import java.util.*;
 
 public class N11650 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        int N = sc.nextInt();
-        ArrayList<ArrayList<Integer>>arr = new ArrayList<>();
-        for (int i=0; i<N;i++){
-            ArrayList<Integer> cor = new ArrayList<>();
-            int x = sc.nextInt();
-            int y = sc.nextInt();
-            cor.add(x);
-            cor.add(y);
-            arr.add(cor);
+        Scanner in = new Scanner(System.in);
+
+        int N = in.nextInt();
+
+        int[][] arr = new int[N][2];
+
+        for(int i = 0; i < N; i++) {
+            arr[i][0] = in.nextInt();
+            arr[i][1] = in.nextInt();
         }
-        Collections.sort(arr, new Comparator<ArrayList<Integer>>() {
-            @Override
-            public int compare(ArrayList<Integer> o1, ArrayList<Integer> o2) {
-                return o1.get(0)==o2.get(0)? o1.get(1)-o2.get(1):o1.get(0)-o2.get(0);
+
+        Arrays.sort(arr, (e1, e2) -> {
+            if(e1[0] == e2[0]) {
+                return e1[1] - e2[1];
+            } else {
+                return e1[0] - e2[0];
             }
         });
-        for(int i = 0; i < arr.size(); i++) {
-            System.out.println(arr.get(i).get(0) + " " + arr.get(i).get(1));
+
+        for(int i = 0; i < N; i++) {
+            System.out.println(arr[i][0] + " " + arr[i][1]);
         }
     }
 }
