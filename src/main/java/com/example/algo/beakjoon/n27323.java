eb234c403052b602fa2de14d0fcd3325b0a17125
SeungchanKKK <eccsck@gmail.com>
2023-09-04 02:19:39 +0900

    [백준] 27323 직사각형 정답제출

---
+++ src/main/java/com/example/algo/beakjoon/n27323.java
@@ -0,0 +1,13 @@
+package com.example.algo.beakjoon;
+
+import java.util.Scanner;
+
+public class n27323 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        System.out.println(A*B);
+    }
+}
