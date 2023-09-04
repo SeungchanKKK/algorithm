27c8919d7bdf4b833168b5bc4e67776c5b087552
SeungchanKKK <eccsck@gmail.com>
2023-09-04 17:23:59 +0900

    [백준] 15894 수학은 체육과목입니다 정답제출

---
+++ src/main/java/com/example/algo/beakjoon/n15894.java
@@ -0,0 +1,11 @@
+package com.example.algo.beakjoon;
+
+import java.util.Scanner;
+
+public class n15894 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long x = sc.nextLong();
+        System.out.println(x*4);
+    }
+}
