5eab7525695703fefa262df826ed2d12092d1cb3
seungchanKKK <eccsck@gmail.com>
2023-09-20 05:43:36 +0900

    [백준] 11651 좌표 정렬하기 2 정답제출

---
+++ src/main/java/com/example/algo/beakjoon/N11651.java
@@ -0,0 +1,28 @@
+package com.example.algo.beakjoon;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+//좌표 정렬하기 2
+public class N11651 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[][] arr = new int[N][2];
+        for (int i = 0; i < N; i++) {
+            arr[i][0] = sc.nextInt();
+            arr[i][1] = sc.nextInt();
+        }
+        Arrays.sort(arr, (e1, e2) -> {
+            if (e1[1] == e2[1]) {
+                return e1[0] - e2[0];
+            } else {
+                return e1[1] - e2[1];
+            }
+        });
+
+        for (int i = 0; i < N; i++) {
+            System.out.println(arr[i][0] + " " + arr[i][1]);
+        }
+    }
+}
---
+++ src/main/java/com/example/algo/beakjoon/n24263.java
@@ -0,0 +1,12 @@
+package com.example.algo.beakjoon;
+
+import java.util.Scanner;
+
+public class n24263 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        System.out.println(N);
+        System.out.println(1);
+    }
+}
