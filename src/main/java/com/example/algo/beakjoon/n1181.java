aefe320f47d8ccacbfc746fea5099639f02764fb
seungchanKKK <eccsck@gmail.com>
2023-10-05 21:26:33 +0900

    [백준] 14888 문제 초기화 패스트캠퍼스 강의 1

---
+++ src/main/java/com/example/algo/beakjoon/n1181.java
@@ -0,0 +1,24 @@
+package com.example.algo.beakjoon;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class n1181 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        ArrayList<String> arr = new ArrayList<>();
+        for (int i=0; i<N; i++){
+            String str = sc.next();
+            arr.add(str);
+        }
+        List<String> arr2 = arr.stream().distinct().sorted(Comparator.comparing(String::length).thenComparing(String::compareTo)).collect(Collectors.toList());
+
+        for (int i=0; i<arr2.size(); i++){
+            System.out.println(arr2.get(i));
+        }
+    }
+}
