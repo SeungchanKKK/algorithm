87d894f8486d80d4af54b8fc58edc5e67cd608c1
seungchanKKK <eccsck@gmail.com>
2024-04-04 00:48:38 +0900

    [프로그래머스] 무인도여행 완료 택배상자 풀이중

---
+++ src/main/java/com/example/algo/programmers/DeliveryBox.java
@@ -0,0 +1,12 @@
+package com.example.algo.programmers;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class DeliveryBox {
+    public int solution(int[] order) {
+        int answer = 0;
+        Deque<Integer>line1 = new ArrayDeque<>();
+        return answer;
+    }
+}
