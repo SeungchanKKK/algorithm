08c8526f273b84b084f3fb804a4fe071d9185947
eccsck <eccsck@gmail.com>
2024-04-04 17:05:34 +0900

    [프로그래머스] 택배상자 풀이중

--- src/main/java/com/example/algo/programmers/DeliveryBox.java
+++ src/main/java/com/example/algo/programmers/DeliveryBox.java
@@ -1,12 +1,53 @@
 package com.example.algo.programmers;
 
 import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.Queue;
+import java.util.Stack;
 
 public class DeliveryBox {
+    int answer = 0;
+    Queue<Integer> line1 = new ArrayDeque<>();
+    Stack<Integer> line2 = new Stack<>();
+
+    public static void main(String[] args) {
+        DeliveryBox deliveryBox = new DeliveryBox();
+        int[] n = {4, 3, 2, 10, 6, 12, 13, 8, 7, 9, 11, 1, 5,};
+        System.out.printf(String.valueOf(deliveryBox.solution(n)));
+    }
+
     public int solution(int[] order) {
-        int answer = 0;
-        Deque<Integer>line1 = new ArrayDeque<>();
+        for (int i = 1; i <= order.length; i++) {
+            line1.add(i);
+        }
+        for (int i = 0; i < order.length; i++) {
+            int require = order[i];
+            if (!process(require)) {
+                break;
+            }
+        }
         return answer;
+    }
+
+    public boolean process(int require) {
+        if (!line1.isEmpty()) {
+            if (line1.peek() == require) {
+                line1.poll();
+                answer++;
+                return true;
+            }
+        }
+        if (!line2.isEmpty()) {
+            if (line2.peek() == require) {
+                line2.pop();
+                answer++;
+                return true;
+            }
+        }
+        if (line1.isEmpty()) {
+            return false;
+        } else {
+            line2.push(line1.poll());
+            return process(require);
+        }
     }
 }
