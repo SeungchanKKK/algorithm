6b13c705204fed6b9f02a0d809a80c396a0940b7
eccsck <eccsck@gmail.com>
2024-06-11 14:23:26 +0900

    [백준] 9663 N퀸 DFS 풀이

--- src/main/java/com/example/algo/beakjoon/N9663.java
+++ src/main/java/com/example/algo/beakjoon/N9663.java
@@ -4,37 +4,37 @@ import java.util.Scanner;
 
 public class N9663 {
     static Scanner sc = new Scanner(System.in);
-    static int cnt =0;
-    static int row =1;
-    static int col =1;
-    static int N;
-    static int[] position;
-    static  boolean isAvailable(int[]position,int col,int row){
-        for (int i=1; i<= row; i++){
-            if (position[i]==col){
+    static int cnt = 0;
+    static int N = sc.nextInt();
+    static int[] position = new int[N + 1];
+
+    static boolean isAvailable(int row) {
+        for (int i = 0; i < row; i++) {
+            if (position[i] == position[row]) {
+                return false;
+            } else if (Math.abs(row - i) == Math.abs(position[row] - position[i])){
                 return false;
             }
         }
         return true;
     }
-    static void reFun(int k){
-        if (k==N){
+
+    static void DFS(int row) {
+        if (row == N) {
             cnt++;
-        }else {
-            for (int i=1;i<=N;i++){
-                if(isAvailable(position,i,k)){
-                    position[k]=i;
-                    reFun(k+1);
-                    cnt++;
-                }
-                position[k]=0;
+            return;
+        }
+
+        for (int col = 0; col < N; col++) {
+            position[row]=col;
+            if (isAvailable(row)) {
+                DFS(row + 1);
             }
         }
     }
+
     public static void main(String[] args) {
-        N = sc.nextInt();
-        position= new int[N+1];
-        reFun(1);
+        DFS(0);
         System.out.println(cnt);
     }
 }
