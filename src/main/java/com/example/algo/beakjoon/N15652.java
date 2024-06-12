fd22a71e164f818e48fa345662686fd903932b33
eccsck <eccsck@gmail.com>
2024-06-12 12:06:39 +0900

    [백준] 15652 N과 M DFS 풀이 수정

--- src/main/java/com/example/algo/beakjoon/N15652.java
+++ src/main/java/com/example/algo/beakjoon/N15652.java
@@ -6,30 +6,33 @@ public class N15652 {
     static StringBuilder sb = new StringBuilder();
     static Scanner sc = new Scanner(System.in);
     static int N = sc.nextInt();
-    static int M = sc.nextInt();
-    static int[]selected = new int[M+1];
 
-    static void reFun(int k){
-        if (k==M+1){
-            for (int i=1; i<=M;i++){
-                sb.append(selected[i]).append(" ");
+    static int M  = sc.nextInt();
+
+    static int[]arr = new int[M];
+
+    static int min = 0;
+
+    static void dfs(int k){
+        if (k==M){
+            for (int i=0; i<arr.length; i++){
+                sb.append(arr[i]+" ");
             }
-            sb.deleteCharAt(sb.length()-1);
-            sb.append('\n');
+            sb.append("\n");
         }else {
-            int start = selected[k-1];
-            if (start==0){
-                start=1;
-            }
-            for (int i=start; i<=N; i++){
-                selected[k]=i;
-                reFun(k+1);
-                selected[k]=0;
+            for (int i=1; i<=N; i++){
+                if (i>=min){
+                    min= i;
+                    arr[k]= i;
+                    dfs(k+1);
+                    min =0;
+                }
             }
         }
     }
+
     public static void main(String[] args) {
-        reFun(1);
+        dfs(0);
         System.out.println(sb.toString());
     }
 }
