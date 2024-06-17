fb79da4982b9860579c55b584e5509ae198f5d56
eccsck <eccsck@gmail.com>
2024-06-17 14:27:30 +0900

    [백준] 10825 국영수 정답제출

--- src/main/java/com/example/algo/beakjoon/n10825.java
+++ src/main/java/com/example/algo/beakjoon/n10825.java
@@ -1,21 +1,39 @@
 package com.example.algo.beakjoon;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class n10825 {
     static Scanner sc = new Scanner(System.in);
     static int N = sc.nextInt();
-    static String[] nameArr = new String[N];
-    static int[] korArr = new int[N];
-    static int[] engArr = new int[N];
-    static int[] mathArr = new int[N];
-    static int[] arr = new int[N];
+    static String[][] arr = new String[N][4];
     public static void main(String[] args) {
         for (int i=0; i<N; i++){
-            nameArr[i] = sc.next();
-            korArr[i] = sc.nextInt();
-            engArr[i] = sc.nextInt();
-            mathArr[i] = sc.nextInt();
+            arr[i][0] = sc.next();
+            arr[i][1] = sc.next();
+            arr[i][2] = sc.next();
+            arr[i][3] = sc.next();
+        }
+        Arrays.sort(arr, new Comparator<String[]>() {
+            @Override
+            public int compare(String[] o1, String[] o2) {
+                if(o2[1].equals(o1[1])){
+                    if(o1[2].equals(o2[2])){
+                        if(o1[3].equals(o2[3])){
+                            return o1[0].compareTo(o2[0]);
+                        }
+                        return Integer.parseInt(o2[3])-Integer.parseInt(o1[3]);
+                    }
+
+                    return Integer.parseInt(o1[2])-Integer.parseInt(o2[2]);
+
+                }
+                return Integer.parseInt(o2[1])- Integer.parseInt(o1[1]);
+            }
+        });
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i][0]);
         }
     }
 }
