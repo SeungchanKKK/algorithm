a24aa842a8bc19f98636fb086657bbcc0b60443a
eccsck <eccsck@gmail.com>
2024-06-20 14:48:24 +0900

    [프로그래머스] 기능개발 정답수정

--- src/main/java/com/example/algo/programmers/FuctionDevelopment.java
+++ src/main/java/com/example/algo/programmers/FuctionDevelopment.java
@@ -1,43 +1,72 @@
 package com.example.algo.programmers;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Queue;
 
 public class FuctionDevelopment {
-    public int[] solution(int[] progresses, int[] speeds) {
-        ArrayList<Integer>done = new ArrayList<>();
-        ArrayList<Integer>work = new ArrayList<>();
-        for (int i=0; i<progresses.length; i++){
-            work.add(progresses[i]);
+//    public int[] solution(int[] progresses, int[] speeds) {
+//        ArrayList<Integer>done = new ArrayList<>();
+//        ArrayList<Integer>work = new ArrayList<>();
+//        for (int i=0; i<progresses.length; i++){
+//            work.add(progresses[i]);
+//        }
+//        int add =0;
+//        while (!work.isEmpty()){
+//            for (int i=0; i<work.size(); i++){
+//                work.set(i, work.get(i)+speeds[i+add]);
+//            }
+//            if (work.get(0)>=100){
+//                int finish = 1;
+//                work.remove(0);
+//                add++;
+//                if (work.isEmpty()){
+//                    done.add(finish);
+//                    break;
+//                }
+//                while (work.get(0)>=100){
+//                    work.remove(0);
+//                    finish++;
+//                    add++;
+//                    if (work.isEmpty()){
+//                        break;
+//                    }
+//                }
+//                done.add(finish);
+//            }
+//        }
+//        int[] answer = new int[done.size()];
+//        for (int i=0; i<answer.length; i++){
+//            answer[i]=done.get(i);
+//        }
+//        return answer;
+//    }
+    public ArrayList<Integer> solution(int[] progresses, int[] speeds) {
+        ArrayList<Integer>arrayList = new ArrayList<>();
+        Queue<Integer> queue = new ArrayDeque<>();
+        for (int progress : progresses) {
+            queue.add(progress);
         }
-        int add =0;
-        while (!work.isEmpty()){
-            for (int i=0; i<work.size(); i++){
-                work.set(i, work.get(i)+speeds[i+add]);
-            }
-            if (work.get(0)>=100){
-                int finish = 1;
-                work.remove(0);
-                add++;
-                if (work.isEmpty()){
-                    done.add(finish);
+        int day =0;
+        int idx =0;
+        while (!queue.isEmpty()){
+            day++;
+            int cnt =0;
+            int work = queue.peek()+(day*speeds[idx]);
+            while (work>=100){
+                queue.poll();
+                idx++;
+                cnt++;
+                if (!queue.isEmpty()){
+                    work = queue.peek()+(day*speeds[idx]);
+                }else {
                     break;
                 }
-                while (work.get(0)>=100){
-                    work.remove(0);
-                    finish++;
-                    add++;
-                    if (work.isEmpty()){
-                        break;
-                    }
-                }
-                done.add(finish);
             }
+            if (cnt>0){
+                arrayList.add(cnt);}
         }
-        int[] answer = new int[done.size()];
-        for (int i=0; i<answer.length; i++){
-            answer[i]=done.get(i);
-        }
-        return answer;
+        return arrayList;
     }
 
     public static void main(String[] args) {
