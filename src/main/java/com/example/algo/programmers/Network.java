373235d8619077efa94321009b5112c673f67e20
eccsck <eccsck@gmail.com>
2024-06-27 15:45:46 +0900

    [프로그래머스] 네트워크 풀이중

--- src/main/java/com/example/algo/programmers/Network.java
+++ src/main/java/com/example/algo/programmers/Network.java
@@ -1,19 +1,30 @@
 package com.example.algo.programmers;
 
+import java.util.ArrayList;
+
 public class Network {
-    public int solution(int n, int[][] computers) {
-        int answer = 0;
-        return answer;
+    public static void main(String[] args) {
+        Network network = new Network();
+        network.solution(3, new int[][]{{1,1,0},{1,1,0},{0,0,1}});
     }
-    int idx=0;
-    public void DFS(boolean d,int[][] computers) {
-        if (!d){
-
-            }else {
-            for (int i=0; i<computers.length; i++){
-
+    int answer = 0;
+    ArrayList<Integer>nodes = new ArrayList<>();
+    public int solution(int n, int[][] computers) {
+        for (int i = 0; i < computers.length; i++) {
+            for (int j=i+1; j<computers.length; j++) {
+                    DFS(computers[i][j],computers,j);
             }
         }
+        return answer;
     }
 
-}
+    public void DFS(int d, int[][] computers,int connect) {
+        if (d==0) {
+            answer++;
+        } else {
+            for (int i=connect+1; i<computers.length;i++){
+                DFS(computers[connect][i],computers,i);
+            }
+        }
+        }
+    }
